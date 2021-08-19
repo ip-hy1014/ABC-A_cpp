a8edda4409a7989bfcb1fae7de62c73fcfd7d451
hy <81627289+ip-hy1014@users.noreply.github.com>
2021-08-19 23:26:34 +0900

    Add files via upload

---
+++ ABC074A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  int a;
+  cin >> n >> a;
+  cout << n*n-a << endl;
+}
+
+// 別解
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  int a;
+  cin >> n >> a;
+  cout << std::pow(n,2) - a << endl;
+}
+
+// 別解
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n,a;
+  cin >> n >> a;
+  cout << n*n-a <<endl;
+}
